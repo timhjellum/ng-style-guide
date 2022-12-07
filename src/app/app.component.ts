274475f7b36a09a78c55acde7475a82a09a78de1
thm <timothy.hjellum@eia.gov>
2022-12-07 17:50:16 -0500

    update repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
-import { Component } from "@angular/core"
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-root",
-  template: `
-    <app-header></app-header>
-    <router-outlet></router-outlet>
-    <app-footer> </app-footer>
-  `,
-  styles: []
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = "eia style guide"
+  title = 'style-guide';
 }
